c7e9c68045926b3c7fbf760cf2e7e7799eb700aa
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-10-06 20:41:57 +0300

    fix: change constants

---
+++ src/utils/constants.js
@@ -0,0 +1,80 @@
+import stage1 from '../images/stages/wedding/Img1.jpg';
+import stage2 from '../images/stages/wedding/Img2.jpg';
+import stage3 from '../images/stages/wedding/Img3.jpg';
+import stage4 from '../images/stages/wedding/Img4.jpg';
+import stage5 from '../images/stages/wedding/Img5.jpg';
+import stage6 from '../images/stages/wedding/Img6.jpg';
+import stage7 from '../images/stages/wedding/Img7.jpg';
+import stage8 from '../images/stages/wedding/Img8.jpg';
+import stage9 from '../images/stages/wedding/Img9.jpg';
+import stage10 from '../images/stages/wedding/Img10.jpg';
+import stage11 from '../images/stages/wedding/Img11.jpg';
+
+export const stagesCard = [
+  {
+    id: 1,
+    title: 'Знакомство',
+    subtitle: 'Знакомство, сбор потребностей и пожеланий, первичные предложения по сценарию, концепции.',
+    image: stage1
+  },
+  {
+    id: 2,
+    title: 'Заключение договора',
+    subtitle: 'Заключение договора',
+    image: stage2
+  },
+  {
+    id: 3,
+    title: 'Обсуждение сценария',
+    subtitle: 'Обсуждение сценария, концепции, плана проекта, детального бюджета.',
+    image: stage3
+  },
+  {
+    id: 4,
+    title: 'Проработка концепции',
+    subtitle: 'Проработка концепции свадьбы: Визуальная часть: цвета, дизайн, детали, оформление, одежда гостей и пары. Сценарий: идея и ее раскрытие, подбор развлекательных мероприятий, обсуждение тайминга, зон, локаций.',
+    image: stage4
+  },
+  {
+    id: 5,
+    title: 'Юридическое сопровождение',
+    subtitle: 'Юридическое сопровождение: согласование работы, разработка и подписание договоров с подрядчиками.',
+    image: stage5
+  },
+  {
+    id: 6,
+    title: 'Работа с площадкой',
+    subtitle: 'Работа с площадкой: составление меню, договоренности об оформлении и т.д.',
+    image: stage6
+  },
+  {
+    id: 7,
+    title: 'Подбор команды',
+    subtitle: 'Подбор команды.',
+    image: stage7
+  },
+  {
+    id: 8,
+    title: 'Организационные вопросы',
+    subtitle: 'Организационные вопросы: переговоры с гостями (о подарках, дресс-коде), логистика и решение других задач, которые возникнут в процессе.',
+    image: stage8
+  },
+  {
+    id: 9,
+    title: 'Разработка тайминга',
+    subtitle: 'Разработка тайминга для пары, гостей, подрядчиков. Контроль за ходом его исполнения.',
+    image: stage9
+  },
+  {
+    id: 10,
+    title: 'Координация мероприятия',
+    subtitle: 'Координация мероприятия на каждом из этапов.',
+    image: stage10
+  },
+  {
+    id: 11,
+    title: 'Контроль',
+    subtitle: 'Контроль выдачи фото и видео-материалов согласно сроков.',
+    image: stage11
+  }
+]

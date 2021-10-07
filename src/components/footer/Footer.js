ee3b4f3e91f09b538db728207d00a0b3c0ae0c08
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-10-07 12:35:46 +0300

    feat: add mobile in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Footer.scss';
+import inst from './images/inst.svg';
+import teleg from './images/teleg.svg';
 
 function Footer(){
   return(
@@ -8,13 +10,16 @@ function Footer(){
         <p className="footer__copyright">© 2021 | Все права защищены</p>
         <div className="footer__info">
           <a className="footer__link" href="#">sozdanie@mail.ru</a>
-          <a className="footer__social">
-            <img className="footer__icon" />
+          <a className="footer__social" href="https://t.me/EugeneBelovv">
+            <img className="footer__icon" src={teleg} alt="Телеграм"/>
           </a>
-          <a className="footer__social">
-            <img className="footer__icon" />
+          <a className="footer__social" href="https://www.instagram.com/eug_ene.belov/">
+            <img className="footer__icon" src={inst} alt="Instagram"/>
           </a>
         </div>
+      </div>
+      <div className="footer__container footer__container_mobile">
+        <p className="footer__copyright footer__copyright_mobile">© 2021 | Все права защищены</p>
       </div>
     </footer>
   )
